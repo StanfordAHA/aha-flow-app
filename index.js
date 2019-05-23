60fb5e9e5c9403635c5d1754f074a32864b3b57a
Keyi Zhang <keyi@stanford.edu>
2019-05-23 12:29:24 -0700

    first pass on the app flow

--- index.js
+++ index.js
@@ -3,9 +3,48 @@
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-	// Your code here
-	app.log('Yay, the app was loaded!')
-    var webhook_token = "1";
+	var body_parser = require('body-parser');
+	var rq = require('request-promise-native');
+
+	app.log('Yay, the app was loaded!');
+	const BUILDKITE_TOKEN = process.env.BUILDKITE_TOKEN;
+	const AHA_FLOW_TOKEN = process.env.AHA_FLOW_TOKEN;
+	app.log(BUILDKITE_TOKEN)
+	
+	// this is standard express route to receive buildkite hooks
+	const router = app.route("/aha-flow-app");
+	const title = "GarnetFlow Check"
+	router.use(body_parser.json())
+
+	// add a route
+	router.post("/hook", async (req, res) => {
+		const token = req.headers.authorization;
+		if (token != AHA_FLOW_TOKEN) {
+			res.send(401);
+		}
+		const id = req.body.id;
+		let github = await app.auth(id);
+		const org = req.body.org;
+		const repo = req.body.repo;
+		const head_sha = req.body.head_sha;
+		const status = req.body.status;
+		const conclusion = req.body.conclusion;
+		let summary = "";
+		github.checks.create({
+			owner: org,
+			repo: repo,
+			name: "GarnetFlow",
+			head_sha: head_sha,
+			status: status,
+			conclusion: conclusion,
+			completed_at: new Date().toISOString(),
+			output: {
+				title: title,
+				summary: summary
+			}
+		})
+		res.send("okay")
+	});
 
 	app.on('*', async context => {
 		const pr = context.payload.pull_request;
@@ -16,25 +55,51 @@ module.exports = app => {
 		}
 
 		const sha = head.sha;
-		const number = context.payload.number;
-		const issue = context.issue({sha: sha});
-
-		const org = pr.base.repo.owner.login;
-		const repo = pr.base.repo.name
-		const conclusion = "success"
-		const title = "GarnetFlow Check"
-
-		console.log(repo);
+		const owner = pr.base.repo.owner;
+		const org = owner.login;
+		var repo = pr.base.repo.name;
+		// remove the "-"
+		new_repo_name = repo.replace("-", "_");
 
 		let summary = "";
+
+		// we need to send out a build kite build event
+		var env = {
+			FLOW_ORG: org,
+			FLOW_REPO: repo,
+			FLOW_HEAD_SHA: sha,
+			FLOW_ID: context.payload.installation.id
+		};
+		env[new_repo_name] = sha;
+
+		var options = {
+			method: "POST",
+			url: "https://api.buildkite.com/v2/organizations/stanford-aha/pipelines/garnetflow/builds",
+			body: {
+				commit: "HEAD",
+				branch: "master",
+				message: "PR from " + repo,
+				env: env,
+			},
+			json: true,
+			headers: {
+				Authorization: "Bearer " + BUILDKITE_TOKEN
+			}
+		}
+
+		const result = await rq(options)
+		 .then(function(info) {
+			app.log(info);
+		 })
+		 .catch(function(err) {
+			 app.log(err);
+		 });
 		return context.github.checks.create({
 			owner: org,
 			repo: repo,
 			name: "GarnetFlow",
-			head_sha: pr.head.sha,
-			status: 'completed',
-			conclusion: conclusion,
-			completed_at: new Date().toISOString(),
+			head_sha: sha,
+			status: 'in_progress',
 			output: {
 				title: title,
 				summary: summary
@@ -42,62 +107,4 @@ module.exports = app => {
 		})
 	})
 
-	// this is standard express route to receive buildkite hooks
-	const router = app.route("/flow");
-	router.use(require("express"));
-
-	router.get("/", async (req, res) => {
-    });
-
-	// add a route
-	router.post("/hook", async (req, res) => {
-		// copied from https://github.com/buildkite/lifx-buildkite-build-light-node/
-		// Verify token
-		if (req.headers['x-buildkite-token'] != webhook_token) {
-			console.log("Invalid webhook token");
-			return res.status(401).send('Invalid token');
-		}
-
-		var buildkiteEvent = req.headers['x-buildkite-event'];
-
-		if (buildkiteEvent == 'build.running') {
-			console.log('Build running');
-			post_to_lifx("/v1beta1/lights/" + bulb_selector + "/effects/breathe.json", {
-				power_on:   false,
-				color:      "yellow brightness:5%",
-				from_color: "yellow brightness:35%",
-				period:     5,
-				cycles:     9999,
-				persist:    true
-			});
-		}
-
-		if (buildkiteEvent == 'build.finished') {
-			if (req.body.build.state == 'passed') {
-				console.log("Build passed");
-				post_to_lifx("/v1/lights/" + bulb_selector + "/effects/breathe.json", {
-					power_on:   false,
-					color:      "green brightness:75%",
-					from_color: "green brightness:10%",
-					period:     0.45,
-					cycles:     3,
-					persist:    true,
-					peak:       0.2
-				});
-			} else {
-				console.log("Build failed");
-				post_to_lifx("/v1/lights/" + bulb_selector + "/effects/breathe.json", {
-					power_on:   false,
-					color:      "red brightness:60%",
-					from_color: "red brightness:25%",
-					period:     0.1,
-					cycles:     20,
-					persist:    true,
-					peak:       0.2
-				});
-			}
-		}
-
-		res.send('AOK');
-	});
 }
